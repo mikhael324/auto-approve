d2ef316a1116e7f8032ad239b30824037864aad9
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-08-10 01:12:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 app = Client("my_bot", bot_token="6111248503:AAGaXvVz8MlSB8uwc63m_pIRuxxLNV5ctis")  # Replace "my_bot" with your bot session name
 
 # Automatically approve all join requests
-@app.on_message(filters.chat_join_request)
+@app.on_chat_join_request()
 def approve_all_requests(client, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
